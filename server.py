52559225bf06da843cc8069817dc9b566233d146
YaelNorman1 <YaelNorman2808@gmail.com>
2022-10-11 16:35:35 +0300

    adding player and dream team classes to backend

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 # from multiprocessing.connection import wait
 # from fastapi import Request
+from urllib.request import Request
 from fastapi import FastAPI
 from starlette.responses import FileResponse 
 from fastapi.staticfiles import StaticFiles
 import requests
 import uvicorn
 import json
+from dream_team import Dream_Team
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -17,9 +19,14 @@ teams_id = {
     "suns": "1610612756"
 }
 
+dream_team = Dream_Team
+
+
+
 @app.get("/")
 async def read_index():
     return FileResponse('static/index.html')
+
 
 @app.get("/players/{year}/{team_name}")
 async def get_players_per_year_team(year, team_name):
@@ -33,6 +40,19 @@ async def get_players_per_year_team(year, team_name):
                 players.append(player)
     return json.dumps(players)
     
+
+@app.get("/dream_team")
+def get_dream_team():
+
+
+
+@app.post("/dream_team")
+async def add_player_to_dream_team(request: Request):
+    new_player= await request.json()
+    dream_team.append(new_player)
+
+
+
 
 
 if __name__ == "__main__":
