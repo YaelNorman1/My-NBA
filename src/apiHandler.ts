66ee881d5096eb05a38d0980a6b11811840fc5cb
YaelNorman1 <YaelNorman2808@gmail.com>
2022-10-12 13:24:33 +0300

    add player and show dream team works

--- src/apiHandler.ts
+++ src/apiHandler.ts
@@ -1,15 +1,15 @@
 class APIHandler {
 
     async getDreamTeam() : Promise<Player[]> {
-    
         const dreamTeam = await $.get(`./dream_team`);
-        const playersJson:Player[] = JSON.parse(dreamTeam);     
+        const playersJson:Player[] = dreamTeam.dream_team;     
         const players : Promise<Player[]> = this.savePlayerDreamTeam(playersJson);               
         return players;               
         
     }
 
-    async savePlayerDreamTeam(players: Player[]) : Promise<Player[]> {
+
+    async savePlayerDreamTeam(players: Player[] | any) : Promise<Player[]> {
         const newPlayersArr : Player[]= []
         players.forEach((player: Player)=>{
             newPlayersArr.push(new Player(player.fname, player.lname, player.jerseyNum, player.position, true))
@@ -21,23 +21,23 @@ class APIHandler {
 
     async addPlayerToDreamTeam(player: Player) : Promise<Player []> {
         let sendPlayerData: string;
-        // try{
-            sendPlayerData= await $.post({
-                url: "./dream_team",
-                type: "post",
-                async: false,
-                dataType: "json",
-                contentType: "application/json",
-                data: JSON.stringify(player)              
-            });
-            const playerJson = JSON.parse(sendPlayerData);
-            const newPlayer= this.savePlayerDreamTeam([playerJson])
-            return newPlayer;
-        // } catch (err){
-        //     return {err:err}
-        // }
-        
-        
+        sendPlayerData= await $.post({
+            url: "./dream_team",
+            type: "post",
+            async: false,
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(player)              
+        });
+        const newPlayer= this.savePlayerDreamTeam(sendPlayerData)
+        return newPlayer;
+    }
+
+
+    async removePlayerFromDreamTeam(playerName : object): Promise<Player> {
+        let deletePlayer: string;
+        // deletePlayer= await $.de
+
     }
 
 }
